3052df4d50a79abe3afdb6cc673ac02ac338bb5c
Mateus Cintra <101955322+cintra1@users.noreply.github.com>
2023-11-14 00:58:15 -0300

    Update article.js

--- components/layouts/article.js
+++ components/layouts/article.js
@@ -9,7 +9,7 @@ const variants = {
 }
 
 const Layout = ({ children, title }) => {
-  const t = `${title} - Takuya Matsuyama`
+  const t = `${title} - Mateus Cintra`
   return (
     <motion.article
       initial="hidden"
