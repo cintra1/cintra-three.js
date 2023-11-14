5300df68f2f61b0adac363220b169fa7f1e59c4b
Mateus Cintra <101955322+cintra1@users.noreply.github.com>
2023-11-14 01:00:26 -0300

    Update work.js

--- components/work.js
+++ components/work.js
@@ -5,7 +5,7 @@ import { ChevronRightIcon } from '@chakra-ui/icons'
 export const Title = ({ children }) => (
   <Box>
     <Link as={NextLink} href="/works">
-      Works
+      Portfolio
     </Link>
     <span>
       {' '}
