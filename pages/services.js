4f469bf56e511bc5d24352a82925033c23b035ae
Mateus Cintra <101955322+cintra1@users.noreply.github.com>
2023-11-15 16:13:16 -0300

    Update services.js

--- pages/services.js
+++ pages/services.js
@@ -24,31 +24,31 @@ const Posts = () => (
         <SimpleGrid columns={[1, 2, 2]} gap={6}>
          <Section>
             <WorkGridItem id="site" title="Criação de Website" thumbnail={thumbPortfolio4}>
-              Landing Page para uma agência de vendas de websites.
+              Está precisando de um site completo, totalmente responsivo e do jeito que você sempre quis? Clique aqui!
             </WorkGridItem>
           </Section>
 
           <Section>
             <WorkGridItem id="hospedagem" title="Hospedagem de Website" thumbnail={thumbPortfolio5}>
-              Landing Page para uma agência de vendas de websites.
+              Está precisando hospedar seu site mas não entende de hosting? Clique aqui!
             </WorkGridItem>
           </Section>
 
            <Section>
             <WorkGridItem id="design" title="Design para Rede Sociais" thumbnail={thumbPortfolio7}>
-              Landing Page para uma agência de vendas de websites.
+              Está precisando melhorar o seu perfil ou da sua empresa nas redes sociais para ter mais engajamento? Clique aqui!
             </WorkGridItem>
           </Section>
 
           <Section>
             <WorkGridItem id="arte" title="Criação de Artes, Banners e Flyers" thumbnail={thumbPortfolio8}>
-              Landing Page para uma agência de vendas de websites.
+              Está precisando de alguma arte, banners ou flyers? Clique aqui!
             </WorkGridItem>
           </Section>
 
           <Section>
             <WorkGridItem id="arte" title="Criação de Atalhos Personalizados" thumbnail={thumbPortfolio9}>
-              Landing Page para uma agência de vendas de websites.
+              Atalhos personalizados automatizam e facilitam o dia a dia, simplificando tarefas que antes eram muito repetitivas.
             </WorkGridItem>
           </Section>
          
