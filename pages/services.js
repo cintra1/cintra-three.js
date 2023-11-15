cf903cc7c28d282824c767af62be6b88e6eda267
Mateus Cintra <101955322+cintra1@users.noreply.github.com>
2023-11-15 16:09:36 -0300

    Update services.js

--- pages/services.js
+++ pages/services.js
@@ -2,6 +2,7 @@ import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { GridItem } from '../components/grid-item'
+import { WorkGridItem } from '../components/grid-item'
 import thumbPortfolio from '../public/images/works/banner-opensea.png'
 import thumbPortfolio1 from '../public/images/works/banner-instagram.png'
 import thumbPortfolio2 from '../public/images/works/banner-twitter.png'
@@ -21,33 +22,35 @@ const Posts = () => (
       </Heading>
       <Section delay={0.1}>
         <SimpleGrid columns={[1, 2, 2]} gap={6}>
-          <GridItem
-            title="Criação de Website"
-            thumbnail={thumbPortfolio4}
-            href="https://opensea.io/collection/harunai-surface"
-          />
-           <GridItem
-            title="Hospedagem de Website"
-            thumbnail={thumbPortfolio5}
-            href="https://midjourney.com/home/?callbackUrl=%2Fapp%2F"
-          />  
-            <GridItem
-            title="Design para Rede Sociais"
-            thumbnail={thumbPortfolio7}
-            href="https://mobile.twitter.com/1harunai"
-          />
-            <GridItem
-            title="Criação de Artes, Banners e Flyers"
-            thumbnail={thumbPortfolio8}
-            href="https://mobile.twitter.com/1harunai"
-          />
-            <GridItem
-            title="Criação de Atalhos Personalizados"
-            thumbnail={thumbPortfolio9}
-            href="https://www.instagram.com/harunaijpg/"
-          />
-           
-          
+         <Section>
+            <WorkGridItem id="site" title="Criação de Website" thumbnail={thumbPortfolio4}>
+              Landing Page para uma agência de vendas de websites.
+            </WorkGridItem>
+          </Section>
+
+          <Section>
+            <WorkGridItem id="hospedagem" title="Hospedagem de Website" thumbnail={thumbPortfolio5}>
+              Landing Page para uma agência de vendas de websites.
+            </WorkGridItem>
+          </Section>
+
+           <Section>
+            <WorkGridItem id="design" title="Design para Rede Sociais" thumbnail={thumbPortfolio7}>
+              Landing Page para uma agência de vendas de websites.
+            </WorkGridItem>
+          </Section>
+
+          <Section>
+            <WorkGridItem id="arte" title="Criação de Artes, Banners e Flyers" thumbnail={thumbPortfolio8}>
+              Landing Page para uma agência de vendas de websites.
+            </WorkGridItem>
+          </Section>
+
+          <Section>
+            <WorkGridItem id="arte" title="Criação de Atalhos Personalizados" thumbnail={thumbPortfolio9}>
+              Landing Page para uma agência de vendas de websites.
+            </WorkGridItem>
+          </Section>
          
         </SimpleGrid>
       </Section>
