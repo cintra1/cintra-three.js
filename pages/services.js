a5a5f07cef701aad61093e6bcc3cb57f0bdc2a8a
Mateus Cintra <101955322+cintra1@users.noreply.github.com>
2023-11-15 16:18:16 -0300

    Update services.js

--- pages/services.js
+++ pages/services.js
@@ -22,35 +22,31 @@ const Posts = () => (
       </Heading>
       <Section delay={0.1}>
         <SimpleGrid columns={[1, 2, 2]} gap={6}>
-         <Section>
-            <WorkGridItem id="site" title="Criação de Website" thumbnail={thumbPortfolio4}>
-              
-            </WorkGridItem>
-          </Section>
-
-          <Section>
-            <WorkGridItem id="hospedagem" title="Hospedagem de Website" thumbnail={thumbPortfolio5}>
-             
-            </WorkGridItem>
-          </Section>
-
-           <Section>
-            <WorkGridItem id="design" title="Design para Rede Sociais" thumbnail={thumbPortfolio7}>
-              Está precisando melhorar o seu perfil ou da sua empresa nas redes sociais para ter mais engajamento? Clique aqui!
-            </WorkGridItem>
-          </Section>
-
-          <Section>
-            <WorkGridItem id="arte" title="Criação de Artes, Banners e Flyers" thumbnail={thumbPortfolio8}>
-              Está precisando de alguma arte, banners ou flyers? Clique aqui!
-            </WorkGridItem>
-          </Section>
-
-          <Section>
-            <WorkGridItem id="arte" title="Criação de Atalhos Personalizados" thumbnail={thumbPortfolio9}>
-              Atalhos personalizados automatizam e facilitam o dia a dia, simplificando tarefas que antes eram muito repetitivas.
-            </WorkGridItem>
-          </Section>
+          <GridItem
+            title="Criação de Website"
+            thumbnail={thumbPortfolio4}
+            href="https://opensea.io/collection/harunai-surface"
+          />
+           <GridItem
+            title="Hospedagem de Website"
+            thumbnail={thumbPortfolio5}
+            href="https://midjourney.com/home/?callbackUrl=%2Fapp%2F"
+          />  
+            <GridItem
+            title="Design para Rede Sociais"
+            thumbnail={thumbPortfolio7}
+            href="https://mobile.twitter.com/1harunai"
+          />
+            <GridItem
+            title="Criação de Artes, Banners e Flyers"
+            thumbnail={thumbPortfolio8}
+            href="https://mobile.twitter.com/1harunai"
+          />
+            <GridItem
+            title="Criação de Atalhos Personalizados"
+            thumbnail={thumbPortfolio9}
+            href="https://www.instagram.com/harunaijpg/"
+          />
          
         </SimpleGrid>
       </Section>
